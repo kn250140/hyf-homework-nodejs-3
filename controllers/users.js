66ead7965d700abf5ccd9481bfcdb5fdb99c2b8e
Nikolic <kn250140@ncr.com>
2020-08-27 14:08:13 +0200

    Why Not Step 7 - Anatomy of a query

---
+++ controllers/users.js
@@ -0,0 +1,24 @@
+//exports.findAll = function(req, res){
+  //var user = require('../controllers/sample_data.js')
+  //var user = [];
+  //res.send(user)
+  //}; 
+  exports.findAll = function(req, res){
+    const fs = require('fs')
+    var path = './controllers/sample_data.js';
+    fs.access(path, fs.F_OK, (err) => {
+      if (err) {
+        var user = [];
+        res.send(user);
+        console.error(err)
+        res.status(200).send()
+        //return
+      }else{
+      user = require('../controllers/sample_data.js')
+      res.send(user);}
+      //file exists
+    })
+    
+    
+    }; //shows an empty array if there's no sample_data.js 
+    //available and if it does exists shows file's content
