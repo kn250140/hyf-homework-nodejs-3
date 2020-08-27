e1bed52c6229e25c1270921dfad18eeea13824b2
kn250140 <69011978+kn250140@users.noreply.github.com>
2020-08-27 15:18:59 +0200

    Update 01-user-rest.js

--- cypress/integration/01-user-rest.js
+++ cypress/integration/01-user-rest.js
@@ -1,8 +1,4 @@
-describe('User RESTful', () => {
-
-  it('Should return a list of users if GET /users"', () => {
-
-    cy.request('/users').its('body').should('deep.eq', []);
+@@ -6,4 +6,63 @@ describe('User RESTful', () => {
 
   });
 
